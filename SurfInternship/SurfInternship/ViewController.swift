c5e2c8acf2177ef26bd2e6ab649179ea24ede05d
dolludaa <dolontaeva00@gmail.com>
2023-02-08 03:55:41 +0300

    Xcode proj created

---
+++ SurfInternship/SurfInternship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfInternship
+//
+//  Created by Людмила Долонтаева on 2/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
