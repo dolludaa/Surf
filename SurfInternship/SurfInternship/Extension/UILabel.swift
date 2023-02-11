796881b4acb724c3d4b712ad9c941f30d62a0e76
dolludaa <dolontaeva00@gmail.com>
2023-02-11 20:37:00 +0300

    extensions Button and Label added

---
+++ SurfInternship/SurfInternship/Extension/UILabel.swift
@@ -0,0 +1,32 @@
+//
+//  UILabel.swift
+//  SurfInternship
+//
+//  Created by Людмила Долонтаева on 2/9/23.
+//
+
+import Foundation
+import UIKit
+
+extension UILabel {
+    
+    func setLineSpacing(lineSpacing: CGFloat = 0.0, lineHeightMultiple: CGFloat = 0.0) {
+        
+        guard let labelText = self.text else { return }
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = lineSpacing
+        paragraphStyle.lineHeightMultiple = lineHeightMultiple
+        
+        let attributedString:NSMutableAttributedString
+        if let labelattributedText = self.attributedText {
+            attributedString = NSMutableAttributedString(attributedString: labelattributedText)
+        } else {
+            attributedString = NSMutableAttributedString(string: labelText)
+        }
+        
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, attributedString.length))
+        
+        self.attributedText = attributedString
+    }
+}
